e289436f224138448c0be41c3c6e62394f04d223
Teo-Ziolkowski <teo.ziolkowski1@marist.edu>
2024-07-08 17:05:21 -0400

    got player working

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,20 +14,20 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	if Input.is_action_pressed("move_right"):
-		if GameTimer=<3 and Input.is_action_pressed("move_right"):
+		if $GameTimer.get_time_left() <= 2 and Input.is_action_pressed("move_right"):
 			SPEED = 315
 			#wait 3 seconds
 			SPEED = 300
-			pass
+			
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		pass
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
