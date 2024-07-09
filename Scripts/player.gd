15915719f770fd95e255e00d8f184034e317db3b
Random8927 <ajay.gionta1@marist.edu>
2024-07-09 12:02:50 -0400

    Player dash working

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 300.0
+@export var walkSPEED = 300.0
+@export var dashSPEED =500.0
 @export var JUMP_VELOCITY = -400.0
+var dashing = false
+var SPEED = walkSPEED
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var dash = false
 
 
 func _physics_process(delta):
@@ -13,18 +17,14 @@ func _physics_process(delta):
 	var facing_direction =0
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if Input.is_action_pressed("move_right"):
-		if $GameTimer.get_time_left() <= 2 and Input.is_action_pressed("move_right"):
-			SPEED = 315
-			#wait 3 seconds
-			SPEED = 300
-			
+	move_and_slide()
 
+
+func _input(event):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
@@ -33,4 +33,32 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	# is dashing then return and ignore any input
+	if dashing==true:
+		return # returning gets out of the function immediately and does not process any of the following code
+
+
+	if event.is_action_pressed("move_right"):
+		if $GameTimer.is_stopped():
+			print("one press")
+			$GameTimer.start()
+		else:
+			print("dash")
+			dash = true
+			$GameTimer.stop()
+			$dashTimer.start(2)
+			SPEED=dashSPEED
+	if event.is_action_released("move_right") and $dashTimer.is_stopped():
+		SPEED = walkSPEED
+	if event.is_action_pressed("move_left"):
+		if $GameTimer.is_stopped():
+			print("one press")
+			$GameTimer.start()
+		else:
+			print("dash")
+			dash = true
+			$GameTimer.stop()
+			$dashTimer.start(2)
+			SPEED=dashSPEED
+	if event.is_action_released("move_left") and $dashTimer.is_stopped():
+		SPEED = walkSPEED
